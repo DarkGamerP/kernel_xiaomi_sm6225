6b5ebedce7277299b0e4d592459b60eeff9cf702
Jabiyeff <cebiyevanar@gmail.com>
2025-04-18 12:16:11 +0400

    ksu-next/ Fix up and adapt for spes
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -11,8 +11,6 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #endif
 
-static int cmdline_proc_show(struct seq_file *m, void *v)
-{
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
 		seq_putc(m, '\n');
